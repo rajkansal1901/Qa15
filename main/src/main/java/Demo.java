1a039cc4caf75125c68262a4d8d6832f123e8b1f
Rajat Kansal <rajkansal1901@gmial.com>
2025-03-05 11:30:27 -0500

    added data

--- main/src/main/java/Demo.java
+++ main/src/main/java/Demo.java
@@ -2,7 +2,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Java World");
-
+int y = 45;
         System.out.println("HEllooooo");
     }
 }
