55edfab2215957b16d82fe42db7a9b58b148b985
Rajat Kansal <rajkansal1901@gmial.com>
2025-03-06 06:18:39 -0500

    Revert "added new code"
    This reverts commit 167dcd85fcd6a766e82e5fab7775927e05f81ea9.

--- main/src/main/java/RightHalfPyramid.java
+++ main/src/main/java/RightHalfPyramid.java
@@ -8,7 +8,6 @@ public class RightHalfPyramid {
                     System.out.println();
                     System.out.println("Hi");
                     long l = 45;
-                    System.out.println("Hello World");
                 }
             }
         }
